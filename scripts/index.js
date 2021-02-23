dc3ce245dfd890526845d7184ac6c125bfbdf94c
Esha-Sherring <esherring2016@gmail.com>
2021-02-23 23:50:38 +0530

    safety rules updated

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,14 @@ const accountDetails = document.querySelector('.account-details');
 const setupUI = (user) => {
 if(user)
 {
+  db.collection('users').doc(user.uid).get().then(doc => {
   const html = `
       <div>Logged in as ${user.email}</div>
+      <div>${doc.data().bio}</div>
+
     `;
     accountDetails.innerHTML = html;
+  });
   // toggle user UI elements
   loggedInLinks.forEach(item => item.style.display = 'block');
   loggedOutLinks.forEach(item => item.style.display = 'none');
